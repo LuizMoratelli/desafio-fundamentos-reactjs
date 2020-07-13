a59fb0ece095dd89fda3a516c705fe395dd775a4
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2020-07-13 17:44:24 -0300

    feat(dashboard): get data from api

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,10 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number | string): string => {
+  const convertedValue = Number(value);
+
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(convertedValue);
+};
 
 export default formatValue;
